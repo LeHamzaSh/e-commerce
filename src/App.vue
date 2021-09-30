0f6584ed5ce8f29688b87460dc741a341c528702
Hamza Sheikh <50829597+LeHamzaSh@users.noreply.github.com>
2021-09-30 03:53:51 -0700

    Navbar completed

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
-  <!--<router-link to="/">Home</router-link>
-  <router-link to="/about">About</router-link>-->
-  <nav class="navbar bg-dark">
-    <div class="container-fluid">
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
+  <!-- Navigation Bar -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <!-- Navbar Contents -->
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <a class="navbar-brand justify-content-left" href="#">Hamza's Website</a>
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li class="active nav-item"><router-link to="/"><button class="btn btn-danger">All Items</button></router-link></li>
+          <li class="nav-item"><router-link to="/about"><button class="btn btn-danger">Add Items</button></router-link></li>
+          <li class="nav-item"><button class="btn btn-danger disabled">Categories</button></li>
+        </ul>
       </div>
-    </div>
 
   </nav>
 
@@ -24,6 +27,19 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background-image: url("./assets/bg.jpg");
+}
+
+.navbar-collapse{
+  justify-content: center;
+  padding-left: 15px;
+  padding-right: 15px;
+}
+
+.nav-item, .nav-item :active{
+  justify-content: center;
+  padding-left: 20px;
+  padding-right: 20px;
 }
 
 </style>
