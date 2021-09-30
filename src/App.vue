bc4c400c6547c53b69e72b59a8cff78a12c281fc
Hamza Sheikh <50829597+LeHamzaSh@users.noreply.github.com>
2021-09-30 01:20:14 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <!--<router-link to="/">Home</router-link>
+  <router-link to="/about">About</router-link>-->
+  <nav class="navbar bg-dark">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+      </div>
+    </div>
+
+  </nav>
+
+  <router-view/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+</style>
