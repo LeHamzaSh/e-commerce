36a240ed60f2979727387cd2d9c777e7158aee9a
Hamza Sheikh <50829597+LeHamzaSh@users.noreply.github.com>
2021-09-30 06:35:32 -0700

    form layout done

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <!-- Navigation Bar -->
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
 
       <!-- Navbar Contents -->
       <div class="collapse navbar-collapse show" id="navbarTogglerDemo01">
-        <a class="navbar-brand justify-content-left" href="#">Hamza's Website</a>
+        <a class="navbar-brand justify-content-left" href="#"><b>Hamza's Website</b></a>
         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
           <li class="active nav-item"><router-link to="/"><button class="btn btn-danger">All Items</button></router-link></li>
           <li class="nav-item"><router-link to="/about"><button class="btn btn-danger">Add Items</button></router-link></li>
@@ -27,7 +27,9 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  /*background-image: url("./assets/bg.jpg");*/
+  background-image: url("./assets/bg.jpg");
+  width: 100vw;
+  height: 100vh;
 }
 
 .navbar-collapse{
@@ -41,5 +43,6 @@
   padding-left: 20px;
   padding-right: 20px;
 }
+
 
 </style>
--- src/components/Form.vue
+++ src/components/Form.vue
@@ -2,19 +2,20 @@
 <!-- Form Starts Here -->
     <div class="container-flex">
         <form class="data-entry-form row">
+            <h1 class="heading-title">Add New Items Form</h1>
             <div class="form-group">
                 <label for="id-entry" class="label-config">Enter ID</label>
-                <input type="id" class="form-control">
+                <input type="id" class="form-control" placeholder="Enter Id for the item">
             </div>
 
             <div class="form-group">
                 <label for="id-entry" class="label-config">Enter Name</label>
-                <input type="id" class="form-control">
+                <input type="id" class="form-control" placeholder="Enter Name for the item">
             </div>
 
             <div class="form-group">
-                <label for="id-entry" class="label-config">Enter Image URL</label>
-                <input type="id" class="form-control">
+                <label for="id-entry" class="label-config">Enter Image Url</label>
+                <input type="id" class="form-control" placeholder="Enter Image Url for the item">
             </div>
         </form>
     </div>
@@ -25,18 +26,11 @@
 </script>
 
 <style>
-.container-flex{
-    width: 100vw;
-    height: 100vh;
-    margin-top: 15px;
-    margin-bottom: 15px;
-    margin-left: 10px;
-    margin-right: 10px;
-}
 
 .form-group{
     margin-top: 15px;
     margin-bottom: 15px;
+    margin-left: 10px;
     display: flex;
     text-align: left;
     align-items: center;
@@ -45,11 +39,29 @@
 .label-config{
     height: 30px;
     width: 160px;
-    font-weight: bold;
+    font-weight: bolder;
+    color: whitesmoke;
+    font-size: 14pt;
 }
 
 .form-control{
-    margin-left: 30px;
     height: 50px;
+    width: 80vw;
+    background-color: transparent;
+    text-align: center;
 }
+
+.data-entry-form{
+    width: 100vw;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    font-family: cursive;
+}
+
+.heading-title{
+    color: whitesmoke;
+    font: italic;
+    font-weight: bold;
+}
+
 </style>
